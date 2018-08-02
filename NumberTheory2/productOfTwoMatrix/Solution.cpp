b450350059c01fca3d0b1fadd0a432ce05a9ad80
Aashish <aashish01official@gmail.com>
2018-08-02 22:29:06 +0530

    Product of 2 matrices

---
+++ NumberTheory2/productOfTwoMatrix/Solution.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+int** productOfTwoMatrices(int** p, int m, int k, int** q, int x, int n){
+	if(k!=x){
+		int** r=0;
+		return r;
+	}
+	static int** r= new int*[m];
+	for(int i=0;i<m;i++){
+		r[i]=new int[n];
+	} 
+	for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			r[i][j]=0;
+			for(int l=0;l<k;l++){
+				r[i][j]=r[i][j]+p[i][l]*q[l][j];
+			}
+		}
+	}
+	return r;
+}
+int main(){
+	int m,k;
+	cin>>m>>k;
+	int** p= new int*[m];
+	for(int i=0;i<m;i++){
+		p[i]=new int[k];
+	}
+	for(int i=0;i<m;i++){
+		for(int j=0;j<k;j++){
+			cin>>p[i][j];
+		}
+	}
+	int x,n;
+	cin>>x>>n;
+	int** q= new int*[x];
+	for(int i=0;i<x;i++){
+		q[i]=new int[n];
+	}
+	for(int i=0;i<x;i++){
+		for(int j=0;j<n;j++){
+			cin>>q[i][j];
+		}
+	}
+	int ** r= productOfTwoMatrices(p,m,k,q,x,n);
+	cout<<k<<" "<<x<<"\n";
+	if(r==0){
+		cout<<"Not possible"<<"\n";
+	}else{
+		for(int i=0;i<m;i++){
+		for(int j=0;j<n;j++){
+			cout<<r[i][j]<<" ";
+		}
+		cout<<"\n";
+	}
+	}
+	
+
+}
