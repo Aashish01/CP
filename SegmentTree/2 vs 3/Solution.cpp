cee4b504747c218611ebc47b44489c8413890246
Aashish <aashish01official@gmail.com>
2018-12-28 13:39:49 +0530

    Second approach mod without value

--- SegmentTree/2 vs 3/Solution.cpp
+++ SegmentTree/2 vs 3/Solution.cpp
@@ -1,139 +1,262 @@
 #include <iostream>
-#include <math.h>
-#include <string>
 using namespace std;
 struct Node{
-	int  digit, modulo;
-	long long int val;
+	char type;
+	int digit;
 	bool hold;
 };
-void buildSegmentTree(Node * p ,int* arr, int start, int end, int index){
+void buildSegmentTree(Node * p, int * arr, int start, int end, int index){
 	if(start==end){
 		if(arr[start]==1){
-			p[index].val=1;
-			p[index].modulo= 1%3;
-			
+			p[index].type='b';
+			p[index].digit=1;
+			p[index].hold=false;
 		}else{
-           p[index].val=0;
-			p[index].modulo= 0%3;
+			p[index].type='a';
+			p[index].digit=1;
+			p[index].hold=false;
 		}
-		p[index].digit=1;
-		p[index].hold=false;
 		return;
 	}
 	int mid=(start+end)/2;
 	buildSegmentTree(p,arr,start,mid,2*index);
 	buildSegmentTree(p,arr,mid+1,end,2*index+1);
-	p[index].digit= p[2*index].digit + p[2*index+1].digit;
-	p[index].val= (long long int)(pow(2,p[2*index+1].digit)*p[2*index].val )+ p[2*index+1].val;
-	
+	p[index].digit=p[2*index].digit+p[2*index+1].digit;
 	p[index].hold=false;
-	p[index].modulo= p[index].val%3;
-	cout<<index<<" "<<p[index].val<<" "<<p[index].modulo<<endl;
-	return;
+    if(p[2*index].type=='a' && p[2*index+1].type =='a'){
+       p[index].type='a';
+    }
+    if(p[2*index].type=='a' && p[2*index+1].type =='b'){
+    	p[index].type='b';
+    }
+    if(p[2*index].type=='a' && p[2*index+1].type =='c'){
+    	p[index].type='c';
+    }
+    if(p[2*index].type=='b' && p[2*index+1].type =='a'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='b';
+    	}else{
+           p[index].type='c';
+    	}
+    }
+    if(p[2*index].type=='b' && p[2*index+1].type =='b'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='c';
+    	}else{
+           p[index].type='a';
+    	}
+    }
+    if(p[2*index].type=='b' && p[2*index+1].type =='c'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='a';
+    	}else{
+           p[index].type='b';
+    	}
+    }
+    if(p[2*index].type=='c' && p[2*index+1].type =='a'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='c';
+    	}else{
+           p[index].type='b';
+    	}
+    }
+    if(p[2*index].type=='c' && p[2*index+1].type =='b'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='a';
+    	}else{
+           p[index].type='c';
+    	}
+    }
+    if(p[2*index].type=='c' && p[2*index+1].type =='c'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='b';
+    	}else{
+           p[index].type='a';
+    	}
+    }
+    return;
+}
+void update(Node * p, int start, int end, int index, int givenIndex){
+	if(start==end){
+		if(p[index].type=='a'){
+			p[index].type='b';
+		}
+		return;
+	}
+	int mid=(start+end)/2;
+	if(givenIndex>mid){
+		update(p,mid+1,end,2*index+1,givenIndex);
+	}else{
+		update(p,start,mid,2*index,givenIndex);
+	}
+	p[index].digit=p[2*index].digit+p[2*index+1].digit;
+	p[index].hold=false;
+    if(p[2*index].type=='a' && p[2*index+1].type =='a'){
+       p[index].type='a';
+    }
+    if(p[2*index].type=='a' && p[2*index+1].type =='b'){
+    	p[index].type='b';
+    }
+    if(p[2*index].type=='a' && p[2*index+1].type =='c'){
+    	p[index].type='c';
+    }
+    if(p[2*index].type=='b' && p[2*index+1].type =='a'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='b';
+    	}else{
+           p[index].type='c';
+    	}
+    }
+    if(p[2*index].type=='b' && p[2*index+1].type =='b'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='c';
+    	}else{
+           p[index].type='a';
+    	}
+    }
+    if(p[2*index].type=='b' && p[2*index+1].type =='c'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='a';
+    	}else{
+           p[index].type='b';
+    	}
+    }
+    if(p[2*index].type=='c' && p[2*index+1].type =='a'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='c';
+    	}else{
+           p[index].type='b';
+    	}
+    }
+    if(p[2*index].type=='c' && p[2*index+1].type =='b'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='a';
+    	}else{
+           p[index].type='c';
+    	}
+    }
+    if(p[2*index].type=='c' && p[2*index+1].type =='c'){
+    	if(p[2*index+1].digit%2==0){
+           p[index].type='b';
+    	}else{
+           p[index].type='a';
+    	}
+    }
+    return;
 }
 Node query(Node * p, int l, int r, int start, int end, int index){
 	if(start>r || end <l){
 		Node n1;
 		n1.hold=true;
 		return n1;
-	}else if(start>=l && end<=r){
-		return p[index];
+	}else if(start>=l && end <=r ){
+         return p[index];
 	}else{
-		int mid=(start+end)/2;
-
-		Node n1= query(p,l,r,start,mid, 2*index);
-		Node n2= query(p,l,r,mid+1,end,2*index+1);
 		Node n3;
+		int mid=(start+end)/2;
+		Node n1= query(p,l,r,start,mid,2*index);
+		Node n2= query(p,l,r,mid+1,end,2*index+1);
 		if(n1.hold==true){
-           n3=n2;
-           return n3;
-		}else if(n2.hold==true){
-             n3=n1;
-             return n3;
+			n3=n2;
+			return n3;
+		}else if(n2.hold ==true){
+			n3=n1;
+			return n3;
 		}else{
 			n3.digit= n1.digit+n2.digit;
-			n3.val= (long long int)(pow(2,n2.digit)*n1.val) + n2.val;
 			n3.hold=false;
-			n3.modulo= n3.val%3;
-			return n3;
+			if(n1.type=='a' && n2.type =='a'){
+              n3.type='a';
+            }
+    if(n1.type=='a' && n2.type =='b'){
+    	n3.type='b';
+    }
+    if(n1.type=='a' && n2.type =='c'){
+    	n3.type='c';
+    }
+    if(n1.type=='b' && n2.type =='a'){
+    	if(n2.digit%2==0){
+           n3.type='b';
+    	}else{
+           n3.type='c';
+    	}
+    }
+    if(n1.type=='b' && n2.type =='b'){
+    	if(n2.digit%2==0){
+           n3.type='c';
+    	}else{
+           n3.type='a';
+    	}
+    }
+    if(n1.type=='b' && n2.type =='c'){
+    	if(n2.digit%2==0){
+           n3.type='a';
+    	}else{
+           n3.type='b';
+    	}
+    }
+    if(n1.type=='c' && n2.type =='a'){
+    	if(n2.digit%2==0){
+           n3.type='c';
+    	}else{
+           n3.type='b';
+    	}
+    }
+    if(n1.type=='c' && n2.type =='b'){
+    	if(n2.digit%2==0){
+           n3.type='a';
+    	}else{
+           n3.type='c';
+    	}
+    }
+    if(n1.type=='c' && n2.type =='c'){
+    	if(n2.digit%2==0){
+           n3.type='b';
+    	}else{
+           n3.type='a';
+    	}
+    }
+      return n3;
 		}
 	}
-}
-void update(Node * p, int start, int end, int index, int givenIndex){
-	int mid=(start+end)/2;
-	if(start==end){
-		if(p[index].val==0){
-			p[index].val=1;
 
-		}
-		return;
-	}
-	if(givenIndex>mid){
-		update(p,mid+1,end,2*index+1,givenIndex);
-		p[index].digit= p[2*index].digit + p[2*index+1].digit;
-	p[index].val= (long long int)(pow(2,p[2*index+1].digit)*p[2*index].val )+ p[2*index+1].val;
-	p[index].hold=false;
-	p[index].modulo= p[index].val%3;
-	return;
-	}
-	 if(givenIndex<=mid){
-		update(p,start,mid,2*index,givenIndex);
-		p[index].digit= p[2*index].digit + p[2*index+1].digit;
-	p[index].val= (long long int)(pow(2,p[2*index+1].digit)*p[2*index].val )+ p[2*index+1].val;
-	p[index].hold=false;
-	p[index].modulo= p[index].val%3;
-	return;
-	}
 }
 int main(){
-	
 	ios_base:: sync_with_stdio(false);
 	cin.tie(NULL);
+	cout.tie(NULL);
 	int n;
 	cin>>n;
-	string str;
-	cin>>str;
 	int * arr= new int[n];
 	Node * p= new Node[4*n];
 	for(int i=0;i<n;i++){
-		char ch= str.at(i);
-		if(ch=='0'){
-			arr[i]=0;
-		}else{
-			arr[i]=1;
-		}
+		cin>>arr[i];
 	}
-	// for(int i=0;i<n;i++){
-	// 	cout<<arr[i]<<" ";
-	// }
 	buildSegmentTree(p,arr,0,n-1,1);
 	int m;
 	cin>>m;
 	for(int i=0;i<m;i++){
-		int t;
-		cin>>t;
-		if(t==0){
-			int l, r;
+		int ch;
+		cin>>ch;
+		if(ch==0){
+			int l,r;
 			cin>>l>>r;
 			Node n1= query(p,l,r,0,n-1,1);
-			cout<<n1.modulo<<endl;
+			char ty=n1.type;
+			if(ty=='a'){
+				cout<<0<<endl;
+			}else if(ty=='b'){
+                cout<<1<<endl;
+			}else{
+				cout<<2<<endl;
+			}
 		}else{
-			int l;
-			cin>>l;
-			update(p,0,n-1,1,l);
+			int givenIndex;
+			cin>>givenIndex;
+			update(p,0,n-1,1,givenIndex);
 		}
 	}
 	delete [] p;
 	delete [] arr;
 	return 0;
-
 }
-
-
-
-
-
-
-
-
